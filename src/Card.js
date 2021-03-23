bdb027a9c01d60b7214fc1e7b9ca586a32963236
you@example.com <rahuldoley1011@gmail.com>
2021-03-24 00:55:43 +0530

    deploy

--- src/Card.js
+++ src/Card.js
@@ -3,11 +3,19 @@ import './Card.css'
 
 const Card = ( breedImage ) => {
 
+    let dogImage
+
+    if (breedImage) {
+        dogImage = Object.values(breedImage).map((Image) => (
+            <img className='dib br3 shadow-5' src={Image} />
+        ))
+    } else {
+        dogImage = <h1></h1>
+    }
+
     return (
         <div>
-            {Object.values(breedImage).map((Image) => (
-                <img className='dib br3 shadow-5' src={Image} />
-            ))}
+            {dogImage}
         </div>
     );
 }
